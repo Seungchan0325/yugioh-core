7f8d22de6766482ae7af6739b34757542aac0f9e
Seungchan0325 <imchan0325@gmail.com>
2022-06-25 23:10:48 +0900

    fix format common.h

--- include/common.h
+++ include/common.h
@@ -2,14 +2,16 @@
 
 namespace yugioh_core
 {
-    using int8_t    = char;
-    using uint8_t   = unsigned char;
-    using int16_t   = short;
-    using uint16_t  = unsigned short;
-    using int32_t   = int;
-    using uint32_t  = unsigned int;
-    using int64_t   = long long;
-    using uint64_t  = unsigned long long;
-    using BOOL      = int8_t;
-    using byte      = uint8_t;
+
+using int8_t    = char;
+using uint8_t   = unsigned char;
+using int16_t   = short;
+using uint16_t  = unsigned short;
+using int32_t   = int;
+using uint32_t  = unsigned int;
+using int64_t   = long long;
+using uint64_t  = unsigned long long;
+using BOOL      = int8_t;
+using byte      = uint8_t;
+
 }
